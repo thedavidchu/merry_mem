1045d6876a9aa31ee678a2632afd3f4e1f374e31
griffinandrew <gaheyrich@gmail.com>
2023-11-26 21:12:17 -0500

    compiles without error, not sure what david was saying about run_trace tho

--- src/traces/main.cpp
+++ src/traces/main.cpp
@@ -24,7 +24,7 @@
 
 // structure to pass data to threads
 struct OperationData {
-    enum class OperationType { insert, search, remove };
+    enum OperationType { insert, search, remove };
     OperationType operation;
     KeyType key;
     ValueType value;
@@ -35,19 +35,22 @@ struct ThreadData {
     ParallelRobinHoodHashTable *hashTable;
     OperationData *operations;
     size_t numOperations;
-    int threadID;
+    uint64_t threadID;
 };
 
 
 // Function to convert OperationType to string for better output
 const char* operationToString(OperationData::OperationType op) {
     switch (op) {
-        case OperationData::INSERT:
+        case OperationData::insert: {
             return "insert";
-        case OperationData::SEARCH:
+        }
+        case OperationData::search: {
             return "search";
-        case OperationData::DELETE:
+        }
+        case OperationData::remove: {
             return "remove";
+        }
     }
     return "unknown";
 }
@@ -83,19 +86,23 @@ run_sequential_perf_test(const std::vector<OperationData>& trace)
     // Assuming that you need to insert, search, and remove operations for each element in the trace
     for (const auto& op : trace) {
         switch (op.operation) {
-            case OperationData::OperationType::insert:
+            case OperationData::OperationType::insert:{
                 a.insert(op.key, op.value);
                 break;
-            case OperationData::OperationType::search:
+            }
+            case OperationData::OperationType::search: {
                 // Perform search operation
                 volatile std::optional<ValueType> r = a.search(op.key);
                 break;
-            case OperationData::OperationType::remove:
+            }
+            case OperationData::OperationType::remove: {
                 a.remove(op.key);
                 break;
-            default:
+            }
+            default: {
                 std::cerr << "Unknown operation: " << operationToString(op.operation) << std::endl;
                 break;
+            }
         }
     }
 }
@@ -135,7 +142,7 @@ void
 generate_trace(std::vector<OperationData> &trace)
 {
     // Populate the operations array with a random sequence of operations
-    for (size_t i = 0; i < NUM_ELEM * NUM_OPS; ++i) {
+    for (size_t i = 0; i < NUM_OPS; ++i) {
         trace[i].operation = static_cast<OperationData::OperationType>(i % 3);
         trace[i].key = i % NUM_ELEM;
         trace[i].value = i % NUM_ELEM + 1; //just so different
@@ -144,7 +151,7 @@ generate_trace(std::vector<OperationData> &trace)
 
 
 void
-run_parallel_perf_test(std::vector<OperationData> &trace, int num_threads)
+run_parallel_perf_test(std::vector<OperationData> &trace, uint64_t num_threads)
 {
     ParallelRobinHoodHashTable a;
 
@@ -155,17 +162,17 @@ run_parallel_perf_test(std::vector<OperationData> &trace, int num_threads)
     size_t operations_per_thread = trace.size() / num_threads;
 
     // Launch threads
-    for (unsigned i = 0; i < num_threads; ++i) {
+    for (uint64_t i = 0; i < num_threads; ++i) {
         threadData[i].hashTable = &a;
-        threadData[i].operations = const_cast<OperationData*>(&trace[i * operations_per_thread]);
+        threadData[i].operations = &trace[i * operations_per_thread];
         threadData[i].numOperations = operations_per_thread;
         threadData[i].threadID = i;
         threads.emplace_back(threadFunction, &threadData[i]);
     }
 
     // Wait for threads to finish
-    for (unsigned i = 0; i < num_threads; ++i) {
-       thread.join();
+    for (auto& thread : threads) {
+        thread.join();
     }
 }
 
@@ -174,7 +181,7 @@ int
 main() 
 {
 
-    std::vector<OperationData> trace(NUM_ELEM * NUM_OPS);
+    std::vector<OperationData> trace(NUM_ELEM); //not sure if this is num elemtns * num ops or just num ops
     generate_trace(trace);
 
     clock_t start, end;
