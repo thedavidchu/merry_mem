07f9bf36ac27a8c1df9a54b7d3d02e8c84e3de28
David Chu <thedavid.chu@mail.utoronto.ca>
2023-11-28 17:58:09 -0500

    Add parallel trace runner

--- src/traces/main.cpp
+++ src/traces/main.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <iostream>
 #include <random>
+#include <thread>
 #include <vector>
 
 #include <time.h>
@@ -101,6 +102,54 @@ run_sequential_performance_test(std::vector<Trace> &traces)
             assert(0 && "impossible!");
         }
         }
+    }
+    end_time = clock();
+    double duration_in_seconds = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    std::cout << "Time in sec: " << duration_in_seconds << std::endl;
+}
+
+void
+run_parallel_worker(ParallelRobinHoodHashTable &hash_table,
+                    const std::vector<Trace> &traces, const size_t t_id,
+                    const size_t num_workers)
+{
+    size_t trace_size = traces.size();
+
+    for (size_t i = t_id; i < trace_size; i += num_workers) {
+        Trace &t = traces[i];
+        switch (t.op) {
+        case TraceOperator::insert: {
+            hash_table.insert(t.key, t.value);
+            break;
+        }
+        case TraceOperator::search: {
+            volatile std::optional<ValueType> r = hash_table.search(t.key);
+            break;
+        }
+        case TraceOperator::remove: {
+            hash_table.remove(t.key);
+            break;
+        }
+        default: {
+            assert(0 && "impossible!");
+        }
+        }
+    }
+}
+
+void
+run_parallel_performance_test(const std::vector<Trace> &traces, const size_t num_workers)
+{
+    clock_t start_time, end_time;
+    std::vector<std::thread> workers;
+
+    start_time = clock();
+    ParallelRobinHoodHashTable hash_table;
+    for (size_t i = 0; i < num_workers; ++i) {
+        workers.emplace_back(run_parallel_worker, hash_table, traces, i, num_workers);
+    }
+    for (auto &w : workers) {
+        w.join();
     }
     end_time = clock();
     double duration_in_seconds = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
